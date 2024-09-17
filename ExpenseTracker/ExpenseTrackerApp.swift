26c2b3d41f9be4bd512abcb979487b6eed5d98bf
Yunus Emre <yunus.eminler@gmail.com>
2024-09-17 21:07:56 +0300

    First commit

---
+++ ExpenseTracker/ExpenseTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpenseTrackerApp.swift
+//  ExpenseTracker
+//
+//  Created by Yunus Emre Eminler on 17.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct ExpenseTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
